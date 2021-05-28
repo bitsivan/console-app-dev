98e7be6c01efa6e1ab4944d4f1bdbbcdadf22d22
bitsivan <sergiocal48@hotmail.com>
2021-05-27 21:37:59 -0500

    initial feature

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -116,9 +116,6 @@ namespace ConsoleApp1
             foreach(string _color in colors){
                 Console.WriteLine(_color);
             }
-            Console.WriteLine(colors[1]);
-            Console.WriteLine(colors[2]);
-            
         }
     }
 }
